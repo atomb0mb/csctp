5b81f03e6039ed4b37271318b1bab2194e0332d7
Maanasa <Maanasa@maanasas-mbp>
2019-04-18 18:18:49 -0400

    Issue #13, got all tests except Edit working

--- iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/HCPLaborAndDeliveryStepDefs.java
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/HCPLaborAndDeliveryStepDefs.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import edu.ncsu.csc.itrust2.forms.hcp.ObstetricsRecordForm;
@@ -74,7 +75,7 @@ public class HCPLaborAndDeliveryStepDefs extends CucumberTest {
      *            of last name of patient
      * @throws InterruptedException
      */
-    @Then ( "^(.+) already has an obstetrics record" )
+    @Then ( "^he selects patient (.+)$" )
     public void selectObstetricsPatient ( final String username ) throws InterruptedException {
 
         // wait for the patients to load before searching
@@ -113,12 +114,12 @@ public class HCPLaborAndDeliveryStepDefs extends CucumberTest {
      *            whether yes or no
      * @throws InterruptedException
      */
-    @Then ( "^he enters (.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+)$" )
+    @And ( "^he enters (.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+)$" )
     public void addReport ( final String laborDate, final String laborTime, final String deliveryDate,
             final String deliveryTime, final String type, final String twin, final String first, final String last,
             final int weight, final int height, final int HR, final int systolic, final int diastolic,
             final String twinFirst, final String twinLast, final int twinWeight, final int twinHeight, final int twinHR,
-            final int twinSystolic, final int twinDiastolic ) throws InterruptedException {
+            final int twinSystolic, final int twinDiastolic, final String twinType ) throws InterruptedException {
         waitForAngular();
         driver.findElement( By.id( "displayCreateForm" ) ).click();
         waitForAngular();
@@ -142,14 +143,17 @@ public class HCPLaborAndDeliveryStepDefs extends CucumberTest {
         // Integer.toString( laborDate ) );
         driver.findElement( By.name( "inputdeliverytime" ) ).sendKeys( deliveryTime );
 
+        driver.findElement( By.cssSelector( "input[type=radio][id=" + twin + "]" ) ).click();
+
         driver.findElement( By.cssSelector( "input[type=radio][id=" + type + "]" ) ).click(); // type
                                                                                               // of
                                                                                               // delivery
 
-        driver.findElement( By.cssSelector( "input[type=radio][id=" + twin + "]" ) ).click(); // whether
-                                                                                              // there
-                                                                                              // are
-                                                                                              // twins
+        // driver.findElement( By.cssSelector( "input[type=radio][id=" + twin +
+        // "]" ) ).click(); // whether
+        // there
+        // are
+        // twins
 
         // INFO ON INFANT
         driver.findElement( By.name( "firstname" ) ).clear();
@@ -189,6 +193,7 @@ public class HCPLaborAndDeliveryStepDefs extends CucumberTest {
 
         if ( twin.equals( "Yes" ) ) {
 
+            driver.findElement( By.cssSelector( "input[type=radio][id=" + twinType + "]" ) ).click();
             // INFO ON TWIN
             driver.findElement( By.name( "firstnameTwin" ) ).clear();
             // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
@@ -245,14 +250,223 @@ public class HCPLaborAndDeliveryStepDefs extends CucumberTest {
      * Checks if the changes were allowed to be made
      */
     @Then ( "^Then a success message is displayed (.+)$" )
-    public void hcpSuccessfulObstetricsVisit () {
+    public void successfulLDCreate () {
         // confirm that the message is displayed
         try {
-            driver.findElement( By.name( "success" ) ).getText().contains( "Office visit edited successfully" );
+            driver.findElement( By.name( "success" ) ).getText().contains( "success" );
         }
         catch ( final Exception e ) {
             fail();
         }
+    }
+
+    // *************************BEGIN INVALID CREATE
+    // LD*************************************
+
+    /**
+     * hcp addPregnancy information
+     *
+     * @param year
+     *            of conception
+     * @param weeks
+     *            of pregnant
+     * @param hours
+     *            of labor
+     * @param deliveryType
+     *            of method
+     * @param twins
+     *            whether yes or no
+     * @throws InterruptedException
+     */
+    @And ( "^he incorrectly enters invalid values (.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+)$" )
+    public void addReportInvalid ( final String laborDate, final String laborTime, final String deliveryDate,
+            final String deliveryTime, final String type, final String twin, final String first, final String last,
+            final int weight, final int height, final int HR, final int systolic, final int diastolic,
+            final String twinFirst, final String twinLast, final int twinWeight, final int twinHeight, final int twinHR,
+            final int twinSystolic, final int twinDiastolic, final String twinType ) throws InterruptedException {
+        waitForAngular();
+        driver.findElement( By.id( "displayCreateForm" ) ).click();
+        waitForAngular();
+        driver.findElement( By.name( "inputlabordate" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "inputlabordate" ) ).sendKeys( laborDate );
+
+        driver.findElement( By.name( "inputlabortime" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "inputlabortime" ) ).sendKeys( laborTime );
+
+        driver.findElement( By.name( "inputdeliverydate" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "inputdeliverydate" ) ).sendKeys( deliveryDate );
+
+        driver.findElement( By.name( "inputdeliverytime" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "inputdeliverytime" ) ).sendKeys( deliveryTime );
+
+        driver.findElement( By.cssSelector( "input[type=radio][id=" + twin + "]" ) ).click();
+
+        driver.findElement( By.cssSelector( "input[type=radio][id=" + type + "]" ) ).click(); // type
+                                                                                              // of
+                                                                                              // delivery
+
+        // driver.findElement( By.cssSelector( "input[type=radio][id=" + twin +
+        // "]" ) ).click(); // whether
+        // there
+        // are
+        // twins
+
+        // INFO ON INFANT
+        driver.findElement( By.name( "firstname" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "firstname" ) ).sendKeys( first );
+
+        driver.findElement( By.name( "lastname" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "lastname" ) ).sendKeys( last );
+
+        driver.findElement( By.name( "weight" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "weight" ) ).sendKeys( Integer.toString( weight ) );
+
+        driver.findElement( By.name( "length" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "length" ) ).sendKeys( Integer.toString( height ) );
+
+        driver.findElement( By.name( "heartrate" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "heartrate" ) ).sendKeys( Integer.toString( HR ) );
+
+        driver.findElement( By.name( "systolic" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "systolic" ) ).sendKeys( Integer.toString( systolic ) );
+
+        driver.findElement( By.name( "diastolic" ) ).clear();
+        // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+        // Integer.toString( laborDate ) );
+        driver.findElement( By.name( "diastolic" ) ).sendKeys( Integer.toString( diastolic ) );
+
+        if ( twin.equals( "Yes" ) ) {
+
+            driver.findElement( By.cssSelector( "input[type=radio][id=" + twinType + "]" ) ).click();
+            // INFO ON TWIN
+            driver.findElement( By.name( "firstnameTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "firstnameTwin" ) ).sendKeys( twinFirst );
+
+            driver.findElement( By.name( "lastnameTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "lastnameTwin" ) ).sendKeys( twinLast );
+
+            driver.findElement( By.name( "weightTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "weightTwin" ) ).sendKeys( Integer.toString( twinWeight ) );
+
+            driver.findElement( By.name( "lengthTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "lengthTwin" ) ).sendKeys( Integer.toString( twinHeight ) );
+
+            driver.findElement( By.name( "heartrateTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "heartrateTwin" ) ).sendKeys( Integer.toString( twinHR ) );
+
+            driver.findElement( By.name( "systolicTwin" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "systolicTwin" ) ).sendKeys( Integer.toString( twinSystolic ) );
+
+            driver.findElement( By.name( "diastolic" ) ).clear();
+            // driver.findElement( By.name( "inputlabordate" ) ).sendKeys(
+            // Integer.toString( laborDate ) );
+            driver.findElement( By.name( "diastolicTwin" ) ).sendKeys( Integer.toString( twinDiastolic ) );
+
+        }
+        waitForAngular();
+        driver.findElement( By.id( "submitLD" ) ).click();
+        waitForAngular();
+
+    }
+    // *************************BEGIN VIEW*************************************
+
+    @And ( "^The labor and delivery reports display (.+) (.+) (.+) (.+) (.+) (.+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+) (.+) (\\d+) (\\d+) (\\d+) (\\d+) (\\d+) (.+)$" )
+    public void viewReports ( final String laborDate, final String laborTime, final String deliveryDate,
+            final String deliveryTime, final String type, final String twin, final String first, final String last,
+            final int weight, final int height, final int HR, final int systolic, final int diastolic,
+            final String twinFirst, final String twinLast, final int twinWeight, final int twinHeight, final int twinHR,
+            final int twinSystolic, final int twinDiastolic, final String twinType ) throws InterruptedException {
+
+        waitForAngular();
+        driver.findElement( By.name( "firstname-0" ) ).getText().contains( "first" );
+        driver.findElement( By.name( "lastname-0" ) ).getText().contains( "last" );
+        driver.findElement( By.name( "firstnameTwin-0" ) ).getText().contains( "twinFirst" );
+        driver.findElement( By.name( "lastnameTwin-0" ) ).getText().contains( "twinLast" );
+
+        driver.findElement( By.name( "weight-0" ) ).getText().contains( "weight" );
+        driver.findElement( By.name( "weightTwin-0" ) ).getText().contains( "twinWeight" );
+
+    }
+
+    // *************************BEGIN EDIT LABOR AND DELIVERY
+    // TESTS*************************************
+
+    /**
+     * Logging in as an OBGYN
+     *
+     *
+     */
+    @Given ( "^Given OBGYN logs in using id (.+) password (.+) and goes to Edit Labor Delivery page$" )
+    public void editAsOBGYN ( final String id, final String passwrd ) {
+        attemptLogout();
+
+        driver.get( baseUrl );
+        final WebElement username = driver.findElement( By.name( "username" ) );
+        username.clear();
+        username.sendKeys( id );
+        final WebElement password = driver.findElement( By.name( "password" ) );
+        password.clear();
+        password.sendKeys( passwrd );
+        final WebElement submit = driver.findElement( By.className( "btn" ) );
+        submit.click();
+
+        if ( id.equals( "OGBYN" ) ) {
+            assertEquals( "iTrust2: HCP Home", driver.getTitle() );
+        }
+
+        ( (JavascriptExecutor) driver ).executeScript( "document.getElementById('editLaborDeliveryReport').click();" );
+
+        assertEquals( "iTrust2: Labor and Delivery Reports", driver.getTitle() );
+    }
+
+    /**
+     * HCP Obstetrics select particular patient on radio list
+     *
+     * @param first
+     *            of first name of patient
+     * @param last
+     *            of last name of patient
+     * @throws InterruptedException
+     */
+    @Then ( "^he selects patient to edit(.+)$" )
+    public void selectPatientToEdit ( final String username ) throws InterruptedException {
+
+        // wait for the patients to load before searching
+        waitForAngular();
+        driver.findElement( By.cssSelector( "input[type=radio][id=" + username + "]" ) ).click();
+        waitForAngular();
     }
 
 }
--- iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/HCPLaborAndDelivery.feature
+++ iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/HCPLaborAndDelivery.feature
@@ -10,15 +10,14 @@ Scenario Outline: Create a Patient labor and delivery reports
 	Given OBGYN logs in using id <id> password <pw> and goes to Labor Delivery page
 	Then he selects patient <name>
 	Then <name> already has an obstetrics record
-	And he enters <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic>
+	And he enters <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic> <twinType>
 	And he submits the form
 	Then a success message is displayed <text>
-	Then the system sends him to the screen displays labor and delivery record
 Examples:
-	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |                     text                                  | id    | pw     |
-	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |   Labor and Delivery Report created successfully          | OGBYN | 123456 |
-	| JillBob    | 01/01/2018 | 10:16 AM   | 09/16/2018 | 06:19 PM | Cesarean Section | No     |   Maya     |	Bob	        |  8      |    16    |  100   |  60      |    64     |           |          |            |            |        |              |               |   Labor and Delivery Report created successfully          | OGBYN | 123456 |
-	| ZaraZi     | 04/01/2019 | 11:15 AM   | 10/02/2019 | 03:15 AM | Miscarriage      |        |            |               |         |          |        |          |           |           |          |            |            |        |              |               |   Labor and Delivery Report created successfully          | OGBYN | 123456 |
+	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |                     text                                  | id    | pw     |    twinType    |
+	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |   Labor and Delivery Report created successfully          | OGBYN | 123456 |                |
+	| JillBob    | 01/01/2018 | 10:16 AM   | 09/16/2018 | 06:19 PM | Cesarean Section | No     |   Maya     |	Bob	        |  8      |    16    |  100   |  60      |    64     |           |          |            |            |        |              |               |   Labor and Delivery Report created successfully          | OGBYN | 123456 |                |
+	| ZaraZi     | 04/01/2019 | 11:15 AM   | 10/02/2019 | 03:15 AM | Miscarriage      |        |            |               |         |          |        |          |           |           |          |            |            |        |              |               |   Labor and Delivery Report created successfully          | OGBYN | 123456 |                |
 
 
 Scenario Outline: View a Patient labor and delivery reports
@@ -26,49 +25,43 @@ Scenario Outline: View a Patient labor and delivery reports
 	And Dr Jenkin has logged in and chosen to view a patient labor and delivery record
 	Then he selects patient <name>
 	And he selects date to view <date>
-	Then The labor and delivery reports display <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic>
+	Then The labor and delivery reports display <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic> <twinType>
 	
 Examples:
-	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |  id    | pw     |
-	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |
-
+	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |  id    | pw     |    twinType   |
+	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |        |        |               |
 
 
 Scenario Outline: Edit a Patient labor and delivery reports
-	Given OBGYN logs in using id <id> password <pw> and goes to Labor Delivery page
-	And Dr Jenkin has logged in and chosen to view a patient labor and delivery record
-	Then he selects patient <name>
-	And he selects date to view <date>
-	When labor and delivery reports displays, it shows <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic>
+	Given OBGYN logs in using id <id> password <pw> and goes to Edit Labor Delivery page
+	Then he selects patient to edit <name>
+	When labor and delivery reports displays, it shows <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic> <twinType>
 	Then he edit pre-filled fields <weightNew>	
 
 Examples:
-	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |   weightNew  |  id    | pw     |
-	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |     9        | OGBYN  | 123456 |
+	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |   weightNew  |  id    | pw     |  twinType    |
+	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       |     9        | OGBYN  | 123456 |              |
 
 Scenario Outline: Invalid edit a Patient labor and delivery reports
-	Given OBGYN logs in using id <id> password <pw> and goes to Labor Delivery page
-	And Dr Jenkin has logged in and chosen to view a patient labor and delivery record
-	Then he selects patient <name>
-	And he selects date to view <date>
-	When labor and delivery reports displays, it shows <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic>
+	Given OBGYN logs in using id <id> password <pw> and goes to Edit Labor Delivery page
+	Then he selects patient to edit <name>
+	When labor and delivery reports displays, it shows <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic> <twinType>
 	Then he edit pre-filled fields <weightNew>	
 
 Examples:
-	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |    weightNew   |  id    | pw     |
-	| JillBob    | 01/01/2018 | 10:16 AM   | 09/16/2018 | 06:19 PM | Caesarean Section | No     |   Maya     |	Bob	        |  8      |    16    |  100   |  60      |    64     |           |          |            |            |        |              |               |        -8      | OGBYN  | 123456 |
+	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight  | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |    weightNew   |  id    | pw     |    twinType  |
+	| JillBob    | 01/01/2018 | 10:16 AM   | 09/16/2018 | 06:19 PM | Caesarean Section | No     |   Maya     |	Bob	        |  8      |    16    |  100   |  60      |    64     |           |          |            |            |        |              |               |        -8      | OGBYN  | 123456 |              |
 
 
 Scenario Outline: Create an Invalid Patient labor and delivery reports
 	Given OBGYN logs in using id <id> password <pw> and goes to Labor Delivery page
-	And Dr Jenkin has logged in and chosen to create a patient new labor and delivery record
 	Then he selects patient <name>
 	Then <name> already has an obstetrics record
-	And he enters <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic>
+	And he incorrectly enters invalid values <dateL> <timeL> <dateD> <timeD> <type> <twin> <first> <last> <weight> <height> <HR> <systolic> <diastolic> <twinFirst> <twinLast> <twinWeight> <twinHeight> <twinHR> <twinSystolic> <twinDiastolic> <twinType>
 	And he submits the form
 	
 Examples:
-	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight   | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |  id    | pw     |
-	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  -8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       | OGBYN  | 123456 |
-	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|   8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       | OGBYN  | 123456 |
+	| name       | dateL      | timeL      | dateD      | timeD    | type             | twin   |   first    |   last        | weight   | height   |  HR    | systolic | diastolic | twinFirst | twinLast | twinWeight | twinHeight | twinHR | twinSystolic | twinDiastolic |  id    | pw     |    twinType    |
+	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|  -8      |    14    |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       | OGBYN  | 123456 |                |
+	| AliceSmith | 01/01/2018 | 10:15 AM   | 09/16/2018 | 06:15 PM | Vaginal Delivery | Yes    |   Peach    |	Salinger	|   8      |    a     |  120   |  60      |    64     | April     | Salinger |    8       |      16    |   120  |     60       |      64       | OGBYN  | 123456 |                |
 
